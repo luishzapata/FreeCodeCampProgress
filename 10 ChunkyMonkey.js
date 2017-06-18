8b381d1ce177cd3cb3859034bc29fbc43f1876d8
Luis <luishzapata@gmail.com>
2017-06-18 15:39:21 -0500

    Create 10 ChunkyMonkey.js

---
+++ 10 ChunkyMonkey.js
@@ -0,0 +1,20 @@
+/*
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+CASES:
+chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+*/
+
+function chunkArrayInGroups(arr, size) {
+  var arr2 = [];
+  for (var i = 0; i < arr.length; i+=size) {
+  arr2.push(arr.slice(i , i+size));
+  }
+  return arr2;
+}
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
