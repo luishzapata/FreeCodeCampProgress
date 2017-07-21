24933f62c1c720e8e6b095ac9019dd95acdd7d12
Luis <luishzapata@gmail.com>
2017-07-21 15:38:01 -0500

    Create 09_booWho.js

---
+++ IntermediateAlgorithmScripting/09_booWho.js
@@ -0,0 +1,28 @@
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false.
+
+CASES:
+booWho(true) should return true.
+booWho(false) should return true.
+booWho([1, 2, 3]) should return false.
+booWho([].slice) should return false.
+booWho({ "a": 1 }) should return false.
+booWho(1) should return false.
+booWho(NaN) should return false.
+booWho("a") should return false.
+booWho("true") should return false.
+booWho("false") should return false.
+*/
+function booWho(bool){
+
+	if(typeof(bool) === "boolean"){
+	  bool = true;
+	}
+	else{
+		bool = false;
+	}
+	return bool;
+}
+
+booWho(null);
