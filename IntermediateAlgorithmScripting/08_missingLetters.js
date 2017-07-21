b2b50e8f66b9033c18c71f7696cae60aaa5856c3
Luis <luishzapata@gmail.com>
2017-07-21 13:46:46 -0500

    Update 08_missingLetters.js

--- IntermediateAlgorithmScripting/08_missingLetters.js
+++ IntermediateAlgorithmScripting/08_missingLetters.js
@@ -7,27 +7,33 @@ fearNotLetter("abce") should return "d".
 fearNotLetter("abcdefghjklmno") should return "i".
 fearNotLetter("bcd") should return undefined.
 fearNotLetter("yz") should return undefined.
+
 */
 function fearNotLetter(str) {
 
     var missing = '';
 
     for (i = 0; i < str.length; i++) {
+    	//Check if letter are consecutive by converting to ASCII codes then check if they are more than
+    	//one space appart from eachother
         if ((str.charCodeAt(i + 1) - str.charCodeAt(i)) != 1) {
-            missing = str.charCodeAt(i);
+            missing = str.charCodeAt(i); //hold the ASCII code for the last consecutive letter
+            console.log(missing);
             break;
         }
-
     }
-
-    if (missing >= str.charCodeAt(0) && missing < str.charCodeAt(str.length - 1)) {
-        missing = (String.fromCharCode(missing + 1));
-
-    } else {
+    	//Check if last consecutive character should fit between first and last letters of the string
+    	
+    if (missing > str.charCodeAt(0) && missing < str.charCodeAt(str.length - 1)) {
+        missing = (String.fromCharCode(missing + 1)); //By adding one we get the value of the missing char
+        console.log(missing);
+    } 
+    
+    else {
         missing = undefined;
     }
-
+    console.log(missing);
     return missing;
 }
 
-fearNotLetter('abce');
+fearNotLetter('xyz');
