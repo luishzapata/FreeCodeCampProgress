609bc26d87a4d5500ec84f98f3ff1037ea33dad9
Luis <luishzapata@gmail.com>
2017-07-24 16:57:29 -0500

    Create 10_sortedUnion.js

---
+++ IntermediateAlgorithmScripting/10_sortedUnion.js
@@ -0,0 +1,37 @@
+/*
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+CASES:
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
+uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+*?
+
+function uniteUnique() {
+  // Creates an empty array to store our final result.
+  var finalArray = [];
+
+  // Loop through the arguments object
+  
+  for (var i = 0; i < arguments.length; i++) {
+    var arrayArguments = arguments[i];
+
+    // Loops through the array at hand
+    for (var j = 0; j < arrayArguments.length; j++) {
+      var indexValue = arrayArguments[j];
+
+      // Checks if the value is already on the final array.
+      if (finalArray.indexOf(indexValue) < 0) {
+        finalArray.push(indexValue);
+      }
+    }
+  }
+
+  return finalArray;
+}
+
+
+
+uniteUnique([1, 3, 2], [1, [5]], [2, [4]]);
