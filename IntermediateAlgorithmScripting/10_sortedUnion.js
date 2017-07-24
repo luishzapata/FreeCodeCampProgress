5833831b7ed65583ef2a5d18fc8dff9c218326cc
Luis <luishzapata@gmail.com>
2017-07-24 16:58:18 -0500

    Update 10_sortedUnion.js

--- IntermediateAlgorithmScripting/10_sortedUnion.js
+++ IntermediateAlgorithmScripting/10_sortedUnion.js
@@ -7,7 +7,7 @@ uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
 uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
 uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
 uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
-*?
+*/
 
 function uniteUnique() {
   // Creates an empty array to store our final result.
