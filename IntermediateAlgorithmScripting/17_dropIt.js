95c064ef0a4a50c1c243c1bd9bf141fb4a91661f
Luis <luishzapata@gmail.com>
2017-08-14 22:37:24 -0500

    Create 17_dropIt.js

---
+++ IntermediateAlgorithmScripting/17_dropIt.js
@@ -0,0 +1,30 @@
+/*
+Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
+
+The second argument, func, is a function you'll use to test the first elements of the array to decide if you should drop it or not.
+
+Return the rest of the array, otherwise return an empty array.
+CASES:
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1].
+dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3].
+dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return [].
+dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4].
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2].
+*/
+
+function dropElements(arr, func) {
+  
+  var times = arr.length; // create new var so arr.shift stays the same
+  for (var i = 0; i < times; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
+  return arr;
+}
+
+// test 
+console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));
