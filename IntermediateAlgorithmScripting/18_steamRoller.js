cbe088cfac00a2c438b64754a933e8b395f35c88
Luis <luishzapata@gmail.com>
2017-08-16 17:00:00 -0500

    Create 18_steamRoller.js

---
+++ IntermediateAlgorithmScripting/18_steamRoller.js
@@ -0,0 +1,14 @@
+/*
+Flatten a nested array. You must account for varying levels of nesting.
+CASES:
+steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
+steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
+steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
+*/
+function steamrollArray(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? steamrollArray(toFlatten) : toFlatten);
+  }, []);
+}
+
